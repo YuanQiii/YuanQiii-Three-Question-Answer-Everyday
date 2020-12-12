7a1d09c802b969197ca1957810769d5380689529
YuanQiii <1833386727@qq.com>
2020-12-12 22:24:12 +0800

    说说你对属性data-的理解/你有用过CSS预处理器吗？喜欢用哪个？原理是什么/如何快速让一个数组乱序，写出来

---
+++ 24/如何快速让一个数组乱序，写出来.js
@@ -0,0 +1,9 @@
+function arrShuffle(arr) {
+  for (var i = arr.length - 1; i >= 0; i--) {
+    var randomIndex = Math.floor(Math.random() * (i + 1));
+    var itemAtIndex = arr[randomIndex];
+    arr[randomIndex] = arr[i];
+    arr[i] = itemAtIndex;
+  }
+  return arr;
+}

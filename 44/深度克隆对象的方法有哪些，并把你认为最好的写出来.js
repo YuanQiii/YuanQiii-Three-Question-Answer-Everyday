c272a5e39ea6cc255b7c5b52d030c03233c58f98
YuanQiii <1833386727@qq.com>
2021-01-02 21:48:39 +0800

    说说video标签中预加载视频用到的属性是什么/手写一个满屏品字布局的方案/深度克隆对象的方法有哪些，并把你认为最好的写出来/说说你对http、https、http2的理解

---
+++ 44/深度克隆对象的方法有哪些，并把你认为最好的写出来.js
@@ -0,0 +1,184 @@
+// 一维数组： newArr=[...oldArr]
+// 只包含数据的对象： JSON.parse
+// 复杂对象：递归，解构
+
+var obj1 = {
+  name: '张三',
+  age: 14,
+  friend: ['小红', '小白'],
+  parents: {
+    mother: {
+      name: '李岚',
+      age: 34
+    },
+    father: {
+      name: '张武',
+      age: 35
+    }
+  }
+}
+var obj2 = JSON.parse(JSON.stringify(obj1))
+obj1.mother === obj2.parents.mother //false
+
+
+function deepCopy(newObj, oldObj) {
+  for (var key in oldObj) {
+    var item = oldObj[key]
+    if (item instanceof Array) {
+      newObj[key] = [];
+      deepCopy(newObj[key], item);
+    } else if (item instanceof Object) {
+      newObj[key] = {};
+      deepCopy(newObj[key], item);
+    } else {
+      newObj[key] = item;
+    }
+  }
+  return newObj;
+}
+
+
+// 完整
+const mapTag = '[object Map]';
+const setTag = '[object Set]';
+const arrayTag = '[object Array]';
+const objectTag = '[object Object]';
+const argsTag = '[object Arguments]';
+
+const boolTag = '[object Boolean]';
+const dateTag = '[object Date]';
+const numberTag = '[object Number]';
+const stringTag = '[object String]';
+const symbolTag = '[object Symbol]';
+const errorTag = '[object Error]';
+const regexpTag = '[object RegExp]';
+const funcTag = '[object Function]';
+
+const deepTag = [mapTag, setTag, arrayTag, objectTag, argsTag];
+
+
+function forEach(array, iteratee) {
+  let index = -1;
+  const length = array.length;
+  while (++index < length) {
+    iteratee(array[index], index);
+  }
+  return array;
+}
+
+function isObject(target) {
+  const type = typeof target;
+  return target !== null && (type === 'object' || type === 'function');
+}
+
+function getType(target) {
+  return Object.prototype.toString.call(target);
+}
+
+function getInit(target) {
+  const Ctor = target.constructor;
+  return new Ctor();
+}
+
+function cloneSymbol(targe) {
+  return Object(Symbol.prototype.valueOf.call(targe));
+}
+
+function cloneReg(targe) {
+  const reFlags = /\w*$/;
+  const result = new targe.constructor(targe.source, reFlags.exec(targe));
+  result.lastIndex = targe.lastIndex;
+  return result;
+}
+
+function cloneFunction(func) {
+  const bodyReg = /(?<={)(.|\n)+(?=})/m;
+  const paramReg = /(?<=\().+(?=\)\s+{)/;
+  const funcString = func.toString();
+  if (func.prototype) {
+    const param = paramReg.exec(funcString);
+    const body = bodyReg.exec(funcString);
+    if (body) {
+      if (param) {
+        const paramArr = param[0].split(',');
+        return new Function(...paramArr, body[0]);
+      } else {
+        return new Function(body[0]);
+      }
+    } else {
+      return null;
+    }
+  } else {
+    return eval(funcString);
+  }
+}
+
+function cloneOtherType(targe, type) {
+  const Ctor = targe.constructor;
+  switch (type) {
+    case boolTag:
+    case numberTag:
+    case stringTag:
+    case errorTag:
+    case dateTag:
+      return new Ctor(targe);
+    case regexpTag:
+      return cloneReg(targe);
+    case symbolTag:
+      return cloneSymbol(targe);
+    case funcTag:
+      return cloneFunction(targe);
+    default:
+      return null;
+  }
+}
+
+function clone(target, map = new WeakMap()) {
+
+  // 克隆原始类型
+  if (!isObject(target)) {
+    return target;
+  }
+
+  // 初始化
+  const type = getType(target);
+  let cloneTarget;
+  if (deepTag.includes(type)) {
+    cloneTarget = getInit(target, type);
+  } else {
+    return cloneOtherType(target, type);
+  }
+
+  // 防止循环引用
+  if (map.get(target)) {
+    return target;
+  }
+  map.set(target, cloneTarget);
+
+  // 克隆set
+  if (type === setTag) {
+    target.forEach(value => {
+      cloneTarget.add(clone(value));
+    });
+    return cloneTarget;
+  }
+
+  // 克隆map
+  if (type === mapTag) {
+    target.forEach((value, key) => {
+      cloneTarget.set(key, clone(value));
+    });
+    return cloneTarget;
+  }
+
+  // 克隆对象和数组
+  const keys = type === arrayTag ? undefined : Object.keys(target);
+  forEach(keys || target, (value, key) => {
+    if (keys) {
+      key = value;
+    }
+    cloneTarget[key] = clone(target[key], map);
+  });
+
+  return cloneTarget;
+}
